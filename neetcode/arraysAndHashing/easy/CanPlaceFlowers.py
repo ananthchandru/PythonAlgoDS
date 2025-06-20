a6ed84c4686d693aaa71f03544d98007c074c90d
Anantha Subramanian <41535159+ananthchandru@users.noreply.github.com>
2025-06-20 07:50:35 +0530

    Update CanPlaceFlowers.py

--- neetcode/arraysAndHashing/easy/CanPlaceFlowers.py
+++ neetcode/arraysAndHashing/easy/CanPlaceFlowers.py
@@ -1,16 +1,18 @@
 class CanPlaceFlowers:
+
+    # O(N) space complexity solution
     def canPlaceFlowers(self, flowerbed: [int], n: int) -> bool:
-        #O(N) space complexity solution
-        f = [0] + flowerbed + [0]
-        for i in range(1,len(f)-1):
+       
+        f = [0] + flowerbed + [0]    # to handle [0,0,1] and [1,0,0] situation
+        for i in range(1,len(f)-1):    # skip first and last to check 3 contiguous 0 spots
             if f[i-1]==0 and f[i]==0 and f[i+1]==0:
                 f[i]=1
                 n-=1
         return n<=0
 
+    # O(1) space complexity solution
     def canPlaceFlowers1(self, flowerbed: [int], n: int) -> bool:
-        #O(1) space complexity solution
-
+       
         for i in range(len(flowerbed)):
             if n==0:
                 return True
@@ -28,4 +30,4 @@ print("\n")
 print(obj.canPlaceFlowers1([1,0,0,0,1], 1))
 print(obj.canPlaceFlowers1([1,0,0,0,1], 2))
 print(obj.canPlaceFlowers1([0,0,1,0,0], 1))
-print(obj.canPlaceFlowers1([1,1,0,0,0], 1))
+print(obj.canPlaceFlowers1([1,1,0,0,0], 1)) 
